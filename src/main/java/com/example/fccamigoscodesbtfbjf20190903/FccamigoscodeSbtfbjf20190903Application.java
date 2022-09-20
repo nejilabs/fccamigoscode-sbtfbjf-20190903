9dfa8ba96c2a7bc10ced807310f8826a19e09840
Nelson Alba Jr. 許鑫偉 (Nelly) <nelsonalbajr@yahoo.com>
2022-09-20 21:25:43 +0800

    @e 733

---
+++ src/main/java/com/example/fccamigoscodesbtfbjf20190903/FccamigoscodeSbtfbjf20190903Application.java
@@ -0,0 +1,13 @@
+package com.example.fccamigoscodesbtfbjf20190903;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FccamigoscodeSbtfbjf20190903Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FccamigoscodeSbtfbjf20190903Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/fccamigoscodesbtfbjf20190903/FccamigoscodeSbtfbjf20190903ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fccamigoscodesbtfbjf20190903;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FccamigoscodeSbtfbjf20190903ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
